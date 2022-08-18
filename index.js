e9c0f3785205afae0619aae0ac16b26bb248c2a5
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-08-19 05:27:19 +0530

    day5

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ const app = express();
 
 const PORT = 3000;
 
+// using the public folder at the root of project
+app.use(express.static("public"));
+
+// using the images folder at the route images
+app.use("/images",express.static("images"));
+
+// GET
+app.get('/',(req,res)=>{
+    res.json(data);
+});
+
+
+// POST
+app.post('/create',(req,res)=>{
+    res.send('POST request successful at /create')
+});
+
+// PUT
+app.put('/edit',(req,res)=>{
+    res.send('PUT request successful at /edit')
+});
+
+// DELETE
+app.delete('/delete',(req,res)=>{
+    res.send('DELETE request successful at /delete')
+});
+
 
 
 app.listen(PORT,()=>{
