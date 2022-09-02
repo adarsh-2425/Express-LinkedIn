0d93bcbdf76c16b4aaab1af906fbae75ee948cb0
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-09-02 18:13:21 +0530

    added manual error

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import data from "./data/mock.json" assert { type: 'json' };
 
 
+
+
 const app = express();
 
 const PORT = 3000;
@@ -9,16 +11,30 @@ const PORT = 3000;
 // using the public folder at the root of project
 app.use(express.static("public"));
 
+
+
 // using the images folder at the route images
 app.use("/images",express.static("images"));
+
+// using express.json and express.urlencoded
+
+// app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 // GET
 app.get('/',(req,res)=>{
     res.json(data);
 });
 
+// POST - express.json and express.urlencoded
+app.post("/item", (req,res) => 
+{
+    console.log(req.body);
+    res.send(req.body);
+});
+
 // GET - download method
-app.get('/download',(req,res)=>{
+app.get('/download',(req, res)=>{
     res.download('./images/agile terminology.jpg');
 });
 
@@ -53,6 +69,12 @@ app.delete('/delete',(req,res)=>{
     res.send('DELETE request successful at /delete')
 });
 
+// sending error messages
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something is Broken!!!!")
+});
 
 
 app.listen(PORT,()=>{
